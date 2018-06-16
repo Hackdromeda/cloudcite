5d9dd28f4b1306a9cad37ba54aaffe5dfcb3d9e3
Avi Shah <as0218@pleasantonusd.net>
2018-06-16 02:33:58 -0700

    Updated security headers

--- api/cloudfront-distribution/index.js
+++ api/cloudfront-distribution/index.js
@@ -3,6 +3,15 @@ exports.handler = (event, context, callback) => {
     const response = event.Records[0].cf.response;
     const headers = response.headers;
     
+    headers['report-to'] = [{
+        key:   'Report-To', 
+        value: JSON.stringify({ "group": "csp-endpoint",
+             "max-age": 10886400,
+             "endpoints": [
+               { "url": "https://cloudcite.report-uri.com/r/d/csp/enforce" }
+             ] })
+    }];
+    
     headers['strict-transport-security'] = [{
         key:   'Strict-Transport-Security', 
         value: "max-age=31536000; includeSubdomains; preload"
@@ -10,7 +19,7 @@ exports.handler = (event, context, callback) => {
 
     headers['content-security-policy'] = [{
         key:   'Content-Security-Policy', 
-        value: "default-src 'self'; img-src 'self'; script-src 'self'; style-src 'self' cdn.materialdesignicons.com *.googleapis.com; object-src 'none'; font-src 'self' data:"
+        value: "default-src 'self' *.cloudcite.net *.cloudflare.com *.googleapis.com; img-src 'self' *.gstatic.com gstatic.com *.google.com google.com translate.google.com *.googleapis.com data:; script-src 'self' 'unsafe-eval' translate.google.com *.googleapis.com; style-src 'self' 'unsafe-eval' 'unsafe-inline' data: cdn.materialdesignicons.com *.googleapis.com; object-src 'none'; font-src 'self' *.googleapis.com cdn.materialdesignicons.com data:; report-uri https://cloudcite.report-uri.com/r/d/csp/enforce; report-to csp-endpoint"
     }];
 
     headers['x-content-type-options'] = [{

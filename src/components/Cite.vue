80dc587ae91f3ea0ac0c40ef82bea1fb63a22103
Naval Patel <navalpatel384@outlook.com>
2018-06-21 15:08:12 -0700

    added website input modal

--- src/components/Cite.vue
+++ src/components/Cite.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="container" id="cite">
+    <b-modal :active.sync="websiteModal" id="websiteModal">
+       <div id="websiteInput">
+        <b-field>
+          <input type="email" placeholder="Enter website url" maxlength="2048" autofocus/>
+        </b-field>
+      </div>
+    </b-modal>
     <div id="citeButtonsRow">
-      <a class="button is-rounded" id="citeButton">Website</a>
+      <a class="button is-rounded" id="citeButton" @click="websiteModal = !websiteModal">Website</a>
       <a class="button is-rounded" id="citeButton">Journal</a>
       <a class="button is-rounded" id="citeButton">Book</a>
       <a class="button is-rounded" id="citeButton">Film/Movie</a>
@@ -17,15 +24,46 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Cite extends Vue {
-    
+    data () {
+      return {
+        websiteModal: false
+      }
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-@media (max-width: 767.98px) {
+@media (max-width: 991.97px) {
   #cite {
     text-align: center;
+  }
+  #websiteInput {
+    color: #fff;
+    font-weight: 400;
+    justify-content: center;
+    text-align: center;
+    padding: 20px;
+
+    input {
+      background-color: transparent; 
+      border-radius: 10px;
+      border-color: transparent;
+      font-size: 2em;
+      color: #fff;
+      border-bottom-color: #fff;
+      caret-color: #fff;
+    }
+    input:focus {
+      outline: none;
+    }
+    input::placeholder {
+      color: #fff;
+      opacity: 0.3;
+    }
+  }
+  #websiteModal {
+    background-color: transparentize($color: #005eea, $amount: 0.7)
   }
   #citeButtonsRow {
     justify-content: left;
@@ -49,6 +87,30 @@ export default class Cite extends Vue {
   #cite {
     text-align: center;
   }
+  #websiteInput {
+    color: #fff;
+    font-weight: 400;
+    justify-content: center;
+    text-align: center;
+    padding: 20px;
+
+    input {
+      background-color: transparent; 
+      border-radius: 10px;
+      border-color: transparent;
+      font-size: 4em;
+      color: #fff;
+      border-bottom-color: #fff;
+      caret-color: #fff;
+    }
+    input:focus {
+      outline: none;
+    }
+    input::placeholder {
+      color: #fff;
+      opacity: 0.3;
+    }
+  }
   #citeButtonsRow {
     justify-content: center;
     display: flex;
@@ -66,6 +128,8 @@ export default class Cite extends Vue {
   #citeButton:hover {
     opacity: 0.9;
   }
-
+  #websiteModal {
+    background-color: transparentize($color: #005eea, $amount: 0.7)
+  }
 }
 </style>

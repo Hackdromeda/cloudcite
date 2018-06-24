20d21273178c03ff892e233b2d660b3f282a10f4
Naval Patel <navalpatel384@outlook.com>
2018-06-23 19:41:16 -0700

    added the WebsiteCitation class

---
+++ src/WebsiteCitation.ts
@@ -0,0 +1,24 @@
+export default class WebsiteCitation {
+    authors: any[]
+    source: string
+    publisher: string
+    published: object
+
+    constructor(authors: any, source: any, publisher: any, published: any) {
+        this.authors = authors
+        this.source = source
+        this.publisher = publisher
+        this.published = published
+    }
+
+    removeAuthor(index: number) {
+        this.authors = this.authors.filter(element => element !== this.authors[index]);
+    }
+    clearAuthor(index: number) {
+        this.authors[index] = {
+            first: '',
+            middle: '',
+            last: ''
+        }
+    }
+}
--- src/views/EditWebsite.vue
+++ src/views/EditWebsite.vue
@@ -1,52 +1,79 @@
 <template>
     <div id="editwebsite">
-        <div class="box" id="editwebsitebox">
             <h1 id="editFormTitle" class="title is-size-4">Edit Website Citation</h1>
-            <b-field grouped>
-                <b-field label="Author's First Name" expanded>
-                    <b-input></b-input>
+            <div class="container" id="editForm">
+                <b-field grouped v-if="websiteCitationData.authors.length == 0">
+                    <b-field expanded>
+                        <b-input placeholder="Author First Name" v-model="websiteCitationData.authors[0].first"></b-input>
+                    </b-field>
+                    <b-field expanded>
+                        <b-input placeholder="Author Middle Name" v-model="websiteCitationData.authors[0].middle"></b-input>
+                    </b-field>
+                    <b-field expanded>
+                        <b-input placeholder="Author Last Name" v-model="websiteCitationData.authors[0].last"></b-input>
+                    </b-field>
+                    <b-field expanded>
+                        <a id="removeAuthorButton" @click="websiteCitationData.clearAuthor(i)"><b-icon icon="minus-circle" custom-size="mdi-24px"></b-icon></a>
+                        <a id="addAuthorButton" @click="websiteCitationData.authors.push({first: null, middle: null, last: null})"><b-icon icon="plus-circle" custom-size="mdi-24px"></b-icon></a>
+                    </b-field>
                 </b-field>
-                <b-field label="Author's Middle Name" expanded>
-                    <b-input></b-input>
+                <b-field grouped v-for="(author, i) in websiteCitationData.authors" :key="i">
+                    <b-field expanded>
+                        <b-input placeholder="Author First Name" v-model="author.first"></b-input>
+                    </b-field>
+                    <b-field expanded>
+                        <b-input placeholder="Author Middle Name" v-model="author.middle"></b-input>
+                    </b-field>
+                    <b-field expanded>
+                        <b-input placeholder="Author Last Name" v-model="author.last"></b-input>
+                    </b-field>
+                    <b-field expanded>
+                        <a id="removeAuthorButton" @click="websiteCitationData.removeAuthor(i)"><b-icon icon="minus-circle" custom-size="mdi-24px"></b-icon></a>
+                        <a id="addAuthorButton" @click="websiteCitationData.authors.push({first: null, middle: null, last: null})"><b-icon icon="plus-circle" custom-size="mdi-24px"></b-icon></a>
+                    </b-field>
                 </b-field>
-                <b-field label="Author's Last Name" expanded>
-                    <b-input></b-input>
-                </b-field>
-            </b-field>
-        </div>
+            </div>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import WebsiteCitation from '../WebsiteCitation';
 
 @Component({
   components: {
   },
 })
-export default class EditWebsite extends Vue {}
+export default class EditWebsite extends Vue {
+    websiteCitationData = new WebsiteCitation([{}], null, null, null)
+
+}
 </script>
 
 <style scoped lang="scss">
 #editFormTitle {
-    color: #163b72;
+    color: #005eea;
 }
-@media (max-width: 991.97px) {
-    #editwebsite {
-        padding: 1px;
-        margin-left: 5%;
-        margin-right: 5%;
+#editwebsite {
+        padding: 10px;
+        min-height: 100vh;
         text-align: center;
         justify-content: center;
-    }
+        background-color: #fff;
+}
+#removeAuthorButton {
+    color: red;
+}
+#addAuthorButton {
+    color: #005eea;
+}
+@media (max-width: 991.97px) {
+
 }
 @media (min-width: 991.98px) {
-    #editwebsite {
-        padding: 10px;
-        margin-left: 15%;
-        margin-right: 15%;
-        text-align: left;
-        justify-content: center;
+    #editForm {
+        padding-left: 20%;
+        padding-right: 20%;
     }
 }
 </style>

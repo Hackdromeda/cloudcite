ab8f6eaad276b322b04f578bc9c31290b9dffe21
Naval Patel <navalpatel384@outlook.com>
2018-07-08 14:07:08 -0700

    updated vuex store

--- src/store.ts
+++ src/store.ts
@@ -5,12 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    
+    citations: []
   },
   mutations: {
-
+    addCitation(state: any, payload: object) {
+      state.citations.push(payload)
+    }
   },
   actions: {
-
+    addCitation(context: any, payload: object) {
+      context.commit('addCitation', payload)
+    }
+  },
+  getters: {
+    getCitations(state: any) {
+      return state.citations
+    }
   }
 })

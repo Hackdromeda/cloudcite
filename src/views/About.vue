8213f5d0c7e9f31094d9a871634f1bc5ee023a7f
Naval Patel <navalpatel384@outlook.com>
2018-07-31 00:14:26 -0700

    updated About Us page

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,7 +8,28 @@
         </h2>
       </div>
     </div>
-    <div id="about"></div>
+    <div id="about">
+      <sui-grid :columns="2" stackable>
+        <sui-grid-row>
+          <sui-grid-column :computer="5" :tablet="8" :mobile="14"/>
+          <sui-grid-column :computer="6" :tablet="8" :mobile="14">
+            <div style="display: inline-flex;">
+            <div style="padding-right: 25px; padding-bottom: 5px;">
+              <img src="/static/images/Naval.jpg" width="128" height="128"/>
+              <h4>Naval Patel</h4>
+              <p>Project co-founder and web developer. You can learn more about me on <a href="https://navalpatel.me/" target="_blank">my website</a>.</p>
+            </div>
+            <div style="padding-left: 25px; padding-bottom: 5px;">
+              <img src="/static/images/Avi.jpg" width="128" height="128"/>
+              <h4>Avi Shah</h4>
+              <p>Project co-founder and backend developer. You can learn more about me on <a href="https://avimshah.tk/#about" target="_blank">my website</a>.</p>
+            </div>
+            </div>
+          </sui-grid-column>
+          <sui-grid-column :computer="5"/>
+        </sui-grid-row>
+      </sui-grid>
+    </div>
   </div>
 </template>
 
@@ -29,5 +50,19 @@ export default class About extends Vue {}
     text-align: center;
     justify-content: center;
     background-color: #fff;
+    color: #000;
+
+    h1 {
+      color: #000;
+    }
+    h2 {
+      color: #000;
+    }
+    h3 {
+      color: #000;
+    }
+    p {
+      text-align: left;
+    }
   }
 </style>

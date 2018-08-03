99b37f03b986e3e73e118ea791d6826a6cc5c73b
Avi Shah <as0218@pleasantonusd.net>
2018-08-03 02:41:18 -0700

    Fix build

--- vue.config.js
+++ vue.config.js
@@ -10,27 +10,30 @@ module.exports = {
         new PrerenderSPAPlugin({
           staticDir: path.resolve(__dirname, 'dist'),
           // Add routes as we develop them
-          routes: ['/', '/about/', '/pricing/', '/privacy/', '/callback/', '/edit/format/website/', '/edit/format/book/', '/edit/format/film/'],
+          // , '/bibliography/', '/cite/website/', '/cite/book/', '/cite/film/'
+          routes: ['/', '/about/', '/pricing/', '/privacy/'],
           postProcess: function (context) {
             var titles = {
               '/': 'CloudCite · The Best Free Automatic Bibliography Generator · MLA, APA, Chicago, Harvard Citation Styles',
               '/about/': 'CloudCite · About',
+              '/bibliography/': 'CloudCite · Bibliography',
               '/pricing/': 'CloudCite · Pricing',
               '/privacy/': 'CloudCite · Privacy',
               '/callback/': 'CloudCite · Log In',
-              '/edit/format/website/': 'CloudCite · Cite a Website · MLA, APA, Chicago, Harvard Citation Styles',
-              '/edit/format/book/': 'CloudCite · Cite a Book · MLA, APA, Chicago, Harvard Citation Styles',
-              '/edit/format/film/': 'CloudCite · Cite a Film · MLA, APA, Chicago, Harvard Citation Styles'
+              '/cite/website/': 'CloudCite · Cite a Website · MLA, APA, Chicago, Harvard Citation Styles',
+              '/cite/book/': 'CloudCite · Cite a Book · MLA, APA, Chicago, Harvard Citation Styles',
+              '/cite/film/': 'CloudCite · Cite a Film · MLA, APA, Chicago, Harvard Citation Styles'
             }
             var descriptions = {
               '/': 'CloudCite is a free, automatic, and ad-free bibliography generator for popular citation styles such as MLA 8th Edition, APA, and Chicago.',
               '/about/': 'Learn about CloudCite. A free, secure and ad-free Bibliography Generator.',
+              '/bibliography/': 'Let CloudCite handle generating bibliographies and filling in fields for you.',
               '/pricing/': 'CloudCite is always free. No ads.',
               '/privacy/': 'CloudCite is committed to user privacy. No ads. No tracking.',
               '/callback/': 'CloudCite is securely logging you in.',
-              '/edit/format/website/': 'CloudCite can create website citations.',
-              '/edit/format/book/': 'CloudCite can create book citations.',
-              '/edit/format/film/': 'CloudCite can create film citations.'
+              '/cite/website/': 'CloudCite can create website citations.',
+              '/cite/book/': 'CloudCite can create book citations.',
+              '/cite/film/': 'CloudCite can create film citations.'
             }
             context.html = context.html.replace(
               /<title>[^<]*<\/title>/i,

ff4504b69c1d30f26aa9e5a7e80c9f3f8dec91d6
Avi Shah <as0218@pleasantonusd.net>
2018-08-09 00:41:59 -0700

    Added pages to film citation and images

--- src/views/CiteFilm.vue
+++ src/views/CiteFilm.vue
@@ -20,30 +20,31 @@
                 <moon-loader style="position: relative; margin-top: 10vh; left: 50%; right: 50%; transform: translateX(-30px)" :loading="isFetching" color="#005eea"></moon-loader>
             </div>
 
-            <sui-grid :columns="3">
-                <sui-grid-row>
-                    <sui-grid-column :mobile="2" :tablet="2" :computer="5"/>
-                    <sui-grid-column :mobile="12" :tablet="12" :computer="6">
-                    <sui-item-group style="text-align: left; padding: 5vh;" divided v-for="(film, i) in filmData" :key="i">
-                        <sui-item>
-                            <sui-item-content>
-                                <sui-item-header v-if="film.title" v-cloak>{{ film.title }}</sui-item-header>
-                                <sui-item-meta>
-                                    <span v-if="film.release_date && film.vote_average" v-cloak>
-                                        Released at {{ film.release_date }}, rated <b>{{ film.vote_average }}</b>
-                                    </span>
-                                </sui-item-meta>
-                                <sui-item-description v-if="film.overview" v-cloak>
-                                    <p>{{ film.overview }}</p>
-                                </sui-item-description>
-                                <sui-button style="margin-top: 1vh;" @click="citeFilm(film)" type="button"><sui-icon name="pencil alternatice"/>Cite Film</sui-button>
-                            </sui-item-content>
-                        </sui-item>
-                    </sui-item-group>
-                    </sui-grid-column>
-                    <sui-grid-column :mobile="2" :tablet="2" :computer="5"/>
-                </sui-grid-row>
-            </sui-grid>
+            <div class="cardGroup" style="margin-left: 5%; margin-right: 5%; margin-top: 15px; margin-bottom: 15px; word-break: break-all;">
+                <sui-card-group :items-per-row="numOfRows">
+                    <sui-card v-for="(film, i) in filmData" :key="i" class="centered" @click="citeFilm(film)">
+                        <sui-image v-if="film.poster_path" :src="`https://image.tmdb.org/t/p/w200${film.poster_path}`" />
+                        <sui-image v-if="!film.poster_path" src="/static/images/poster-placeholder-min.png" />
+                        <sui-card-content>
+                            <sui-card-header>{{ film.title }}</sui-card-header>
+                        </sui-card-content>
+                    </sui-card>
+                </sui-card-group>
+                <h4 style="font-weight: normal;" v-if="filmData.length == 0 && filmTitle.length > 0 && !isFetching && empty">There were no movies matching your query <b>{{ filmTitle }}.</b></h4>
+            </div>
+            <nav style="margin-left: 5%; margin-right: 5%;" v-if="dataPosition != null && dataPosition.total_pages > 1" class="pagination is-rounded" role="navigation" aria-label="pagination">
+                <a v-if="dataPosition.page > 1" @click="updatePage(dataPosition.page - 1)" class="pagination-previous">Previous</a>
+                <a v-if="dataPosition.page < dataPosition.total_pages" @click="updatePage(dataPosition.page + 1)" class="pagination-next">Next page</a>
+                <ul class="pagination-list">
+                    <li v-if="dataPosition.page > 1"><a @click="updatePage(1)" class="pagination-link" aria-label="Goto page 1">1</a></li>
+                    <li v-if="dataPosition.total_pages - dataPosition.page > 2"><span class="pagination-ellipsis">&hellip;</span></li>
+                    <li v-if="dataPosition.page > 2"><a @click="updatePage(dataPosition.page - 1)" class="pagination-link" aria-label="Goto page">{{ dataPosition.page - 1 }}</a></li>
+                    <li><a class="pagination-link is-current" aria-label="Page" aria-current="page">{{ dataPosition.page }}</a></li>
+                    <li v-if="dataPosition.page + 1 <= dataPosition.total_pages"><a @click="updatePage(dataPosition.page + 1)" class="pagination-link" aria-label="Goto page">{{ dataPosition.page + 1 }}</a></li>
+                    <li v-if="dataPosition.page + 2 < dataPosition.total_pages && dataPosition.page != dataPosition.total_pages"><span class="pagination-ellipsis">&hellip;</span></li>
+                    <li v-if="dataPosition.page != dataPosition.total_pages && dataPosition.page + 1 != dataPosition.total_pages"><a @click="updatePage(dataPosition.total_pages)" class="pagination-link" aria-label="Goto page">{{ dataPosition.total_pages }}</a></li>
+                </ul>
+            </nav>
     </div>
 </template>
 
@@ -72,14 +73,18 @@ import * as  _ from 'lodash/core';
         monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "Month Published"],
         filmData: [],
         filmTitle: '',
+        dataPosition: null,
         selectedFilm: '',
-        isFetching: false
+        isFetching: false,
+        filmPage: 1
       }
   },
   methods: {
       getAsyncData: debounce(function () {
         //@ts-ignore
         this.filmData = []
+        //@ts-ignore
+        this.dataPosition = []
         //@ts-ignore
         this.isFetching = true
         //@ts-ignore
@@ -90,14 +95,24 @@ import * as  _ from 'lodash/core';
             },
             method: 'POST',
             //@ts-ignore
-            body: {"title": this.filmTitle, "format": "movie"},
+            body: {"title": this.filmTitle, "format": "movie", "page": this.filmPage},
             json: true
             //@ts-ignore
         }).then(data => {
                 //@ts-ignore
                 data.results.forEach((item) => this.filmData.push(item))
                 //@ts-ignore
+                this.dataPosition = {"total_results": data.total_results, "page": data.page, "total_pages": data.total_pages}
+                //@ts-ignore
                 this.isFetching = false
+                if(data.length > 0){
+                    //@ts-ignore
+                    this.empty = false;
+                }
+                else{
+                    //@ts-ignore
+                    this.empty = true;
+                }
             })
             //@ts-ignore
             .catch((error) => {
@@ -105,7 +120,7 @@ import * as  _ from 'lodash/core';
                 this.isFetching = false
                 throw error
             })
-    }, 500),
+    }, 250),
     citeFilm: function(film) {
         this.$data.selectedFilm = film;
         if (this.$data.selectedFilm != null && this.$data.selectedFilm != '') {
@@ -150,14 +165,38 @@ import * as  _ from 'lodash/core';
         //@ts-ignore
         this.$store.dispatch('setEditingProject', this.$data.filmCitationData)
         this.$router.push({path: '/edit/film/'})
+    },
+    updatePage(page) {
+        //@ts-ignore
+        this.filmPage = page;
+        //@ts-ignore
+        this.getAsyncData()
     }
   },
+  computed: {
+      numOfRows: function () {
+          //@ts-ignore
+          if(Math.max(document.documentElement.clientWidth, window.innerWidth || 0) >= 850){
+              return 5;
+          }
+          else{
+              return 2
+          }
+      }
+  }
 })
 export default class CiteFilm extends Vue {
 }
 </script>
 
 <style scoped lang="scss">
+@import "~bulma/sass/utilities/_all";
+@import "~bulma/sass/components/pagination.sass";
+
+ul {
+  list-style: none;
+}
+
 #citeFilm {
     min-height: 100vh;
     text-align: center;

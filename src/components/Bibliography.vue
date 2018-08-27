595f37a0e311dc66edf84379e9b856adb970a8b3
Naval Patel <navalpatel384@outlook.com>
2018-08-27 12:39:32 -0700

    bug fixes

--- src/components/Bibliography.vue
+++ src/components/Bibliography.vue
@@ -222,6 +222,8 @@ import _ from 'lodash';
       //@ts-ignore
       var cslData = this.$store.getters.getCitations.filter(citation => citation.id == id)[0]
       //@ts-ignore
+      cslData.contributors = cslData.contributors.map(contributor => _.pickBy(Object.assign(contributor, {given: (contributor.given && contributor.given.split(" ")[0] ? contributor.given.split(" ")[0]: null), middle: (contributor.given && contributor.given.split(" ")[1] ? contributor.given.split(" ")[1]: null)})));
+      //@ts-ignore
       if (cslData && cslData.type && cslData.type == "website") {
         //@ts-ignore
         this.$store.dispatch('setEditingCitation', cslData)

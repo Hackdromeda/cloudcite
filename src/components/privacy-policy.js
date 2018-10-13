caa2d3508563c4b764488683c52b35ae07530741
Naval Patel <navalpatel384@outlook.com>
2018-10-13 14:41:30 -0700

    updated CloudCite to use rel="noreferrer"

--- src/components/privacy-policy.js
+++ src/components/privacy-policy.js
@@ -23,7 +23,7 @@ class PrivacyPolicy extends PageViewElement {
         <p>The Privacy Policy was last updated on August 28, 2018. CloudCite and its developers may without notice to you and at its sole discretion, amend this policy. Please review this policy from time to time. This information may not apply in its entirety to alpha or beta versions of CloudCite.</p>
         <h5>CloudCite takes privacy very seriously. Here are some of the steps we take to protect your privacy:</h6>
         <ol>
-            <li>We use <a href="https://www.websecurity.symantec.com/security-topics/what-is-ssl-tls-https" target="_blank" rel="noopener noreferrer">Secure Sockets Layer (SSL)/Transport Layer Security (TLS)</a> to safeguard your information from being modified while being transferred to and from our APIs, servers, and/or databases.</li>
+            <li>We use <a href="https://www.websecurity.symantec.com/security-topics/what-is-ssl-tls-https" target="_blank" rel="noreferrer">Secure Sockets Layer (SSL)/Transport Layer Security (TLS)</a> to safeguard your information from being modified while being transferred to and from our APIs, servers, and/or databases.</li>
             <li>We use an HTTP Strict Transport Security (HSTS) policy which forces all server, API, and database requests and responses to pass through encrypted Hyper Text Transfer Protocol Secure (HTTPS) connections instead of plain text HTTP.</li>
             <li>We use Auth0 token-based authentication so you can rest assured your password and personal information is protected</li>
             <li>We do not use popup ads or force you to click on ads</li>
@@ -45,7 +45,7 @@ class PrivacyPolicy extends PageViewElement {
             <li><b>Data Portability:</b> CloudCite supports several data export formats. For help with exporting your data, click on the help button on our site or email us at help@cloudcite.net.</li>
             <li><b>Data Deletion:</b> CloudCite can completely delete your account, projects, and any information upon your request. To request data deletion, click on the help button on our site or email us at <a href="mailto:help@cloudcite.net">help@cloudcite.net</a>.</li>
         </ol>
-        <p>By using our service, you hereby consent to our <a href="https://cloudcite.net/privacy/">Privacy Policy</a>, our Terms of Use and agree to any applicable Terms and Conditions including those of the APIs used.</p>
+        <p>By using our service, you hereby consent to our <a href="https://cloudcite.net/privacy/" rel="noreferrer">Privacy Policy</a>, our Terms of Use and agree to any applicable Terms and Conditions including those of the APIs used.</p>
       </section>
     `;
   }

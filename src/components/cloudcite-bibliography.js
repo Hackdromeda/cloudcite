d232f3b45b24e47e111687e72ae3f3da2c9a945e
Naval Patel <navalpatel384@outlook.com>
2018-10-27 23:34:24 -0700

    updated cloudcite-bibliography

--- src/components/cloudcite-bibliography.js
+++ src/components/cloudcite-bibliography.js
@@ -73,59 +73,45 @@ class CloudCiteBibliography extends HTMLElement {
     }
 
     async generatePreview() {
-        let cslObject = await removeEmptyFromObject(this._citationData);
-        for (let i = 0; i < ProjectStore.project.citations.length; i++) {
-            cslObject[ProjectStore.project.citations[i].id] = ProjectStore.project.citations[i];
-        }
-        const generatedHTML = await generateHTML({style: this._citationStyle, locale: this._locale, csl: cslObject, lang: "en-US", cslHTML: this._citationHTML});
-        this._format = generatedHTML.format;
-        this._citationHTML = generatedHTML.html;
-        if (generatedHTML && generatedHTML.error) {
-            console.log(generatedHTML.error)
-        } 
-        else {
+        if (ProjectStore.project.citations.length > 0) {
+            let cslObject = await removeEmptyFromObject(this._citationData);
+            for (let i = 0; i < ProjectStore.project.citations.length; i++) {
+                cslObject[ProjectStore.project.citations[i].id] = ProjectStore.project.citations[i];
+            }
+            const generatedHTML = await generateHTML({style: this._citationStyle, locale: this._locale, csl: cslObject, lang: "en-US", cslHTML: this._citationHTML});
             this._format = generatedHTML.format;
-            if (generatedHTML.html && generatedHTML.html.length > 0) {
-                this._citationHTML = generatedHTML.html.map(htmlItem => htmlItem.html);
-                this._cslBibRef = hyperHTML.wire(this._citationHTML, 'html')`
-                <div>
-                    <div id="copyBibliographyText" style=${{textAlign: 'center', backgroundColor: '#0066ff', borderRadius: '10px', padding: '2px', marginBottom: '3vh', minWidth: '25vh', color: '#ffffff', cursor: 'pointer'}}>
-                        <h2>Copy Bibliography</h2>
-                    </div>
-                    <div style=${{marginTop: '10px', backgroundColor: '#ffffff', border: '1px solid #e0e0e0', padding: '20px', borderRadius: '5px', textAlign: 'left', fontWeight: 'normal !important'}}>
-                        <div class="csl-bib-body" style=${{lineHeight: this._format.linespacing, marginLeft: `${this._format.hangingindent}em`, textIndent: `-${this._format.hangingindent}em`}}>
-                            ${this._citationHTML.map((cslEntry, index) =>
-                                `<div id="cslEntryContainer${index}" style="clear: left; margin-bottom: ${this._format.entryspacing}}">
-                                    ${cslEntry}
-                                </div>
-                                <button id="copyCitationButton${index}">Copy Citation</button>`
-                            )}
+            this._citationHTML = generatedHTML.html;
+            if (generatedHTML && generatedHTML.error) {
+                console.log(generatedHTML.error)
+            } 
+            else {
+                this._format = generatedHTML.format;
+                if (generatedHTML.html && generatedHTML.html.length > 0) {
+                    this._citationHTML = generatedHTML.html.map(htmlItem => htmlItem.html);
+                    this._cslBibRef = hyperHTML.wire(this._citationHTML, 'html')`
+                    <div>
+                        <div id="copyBibliographyText" style=${{textAlign: 'center', backgroundColor: '#0066ff', borderRadius: '10px', padding: '2px', marginBottom: '3vh', minWidth: '25vh', color: '#ffffff', cursor: 'pointer'}}>
+                            <h2>Copy Bibliography</h2>
                         </div>
-                    </div>
-                </div>`;
+                        <div style=${{marginTop: '10px', backgroundColor: '#ffffff', border: '1px solid #e0e0e0', padding: '20px', borderRadius: '5px', textAlign: 'left', fontWeight: 'normal !important'}}>
+                            <div class="csl-bib-body" style=${{lineHeight: this._format.linespacing, marginLeft: `${this._format.hangingindent}em`, textIndent: `-${this._format.hangingindent}em`}}>
+                                ${this._citationHTML.map((cslEntry, index) =>
+                                    `<div id="cslEntryContainer${index}" style="clear: left; margin-bottom: ${this._format.entryspacing}}">
+                                        ${cslEntry}
+                                    </div>
+                                    <button id="copyCitationButton${index}">Copy Citation</button>`
+                                )}
+                            </div>
+                        </div>
+                    </div>`;
 
-                this._cslBibRef.querySelector('#copyBibliographyText').addEventListener('click', (e) => {
-                    let bibliographyContent = "";
-                    for (let i = 0; i < ProjectStore.project.citations.length; i++) {
-                        bibliographyContent += this._cslBibRef.querySelector(`#cslEntryContainer${i}`).innerText;
-                    }
-                    this._textPlain = bibliographyContent;
-                    this._textHTML = `<div class="csl-bib-body" style="${this._format ? (this._format.linespacing != null ? (`line-height:${this._format.linespacing};`): ''): ''}${this._format ? (this._format.hangingindent != null ? (`text-indent:-${this._format.hangingindent}em;`): ''): ''}">${this._citationHTML.map(cslHTMLItem => `<div style="clear:left;${(this._format.entryspacing != null ? (`margin-bottom:${this._format.entryspacing}em;"`): '"')}>${cslHTMLItem}</div>`).join('')}</div>`;
-                    var element = document.createElement('textarea');
-                    element.value = this._textPlain;
-                    this._cslBibRef.appendChild(element);
-                    element.focus({
-                        preventScroll: true
-                    });
-                    element.setSelectionRange(0, element.value.length);
-                    document.execCommand('copy');
-                    this._cslBibRef.removeChild(element);
-                });
-                
-                for (let i = 0; i < ProjectStore.project.citations.length; i++) {
-                    this._cslBibRef.querySelector(`#copyCitationButton${i}`).addEventListener('click', (e) => {
-                        this._textPlain = this._cslBibRef.querySelector(`#cslEntryContainer${i}`).innerText;
-                        this._textHTML = `<div class="csl-bib-body" style="${this._format ? (this._format.linespacing != null ? (`line-height:${this._format.linespacing};`): ''): ''}${this._format ? (this._format.hangingindent != null ? (`text-indent:-${this._format.hangingindent}em;`): ''): ''}"><div style="clear:left;${(this._format.entryspacing != null ? (`margin-bottom:${this._format.entryspacing}em;"`): '"')}>${this._citationHTML[i]}</div></div>`;
+                    this._cslBibRef.querySelector('#copyBibliographyText').addEventListener('click', (e) => {
+                        let bibliographyContent = "";
+                        for (let i = 0; i < ProjectStore.project.citations.length; i++) {
+                            bibliographyContent += this._cslBibRef.querySelector(`#cslEntryContainer${i}`).innerText;
+                        }
+                        this._textPlain = bibliographyContent;
+                        this._textHTML = `<div class="csl-bib-body" style="${this._format ? (this._format.linespacing != null ? (`line-height:${this._format.linespacing};`): ''): ''}${this._format ? (this._format.hangingindent != null ? (`text-indent:-${this._format.hangingindent}em;`): ''): ''}">${this._citationHTML.map(cslHTMLItem => `<div style="clear:left;${(this._format.entryspacing != null ? (`margin-bottom:${this._format.entryspacing}em;"`): '"')}>${cslHTMLItem}</div>`).join('')}</div>`;
                         var element = document.createElement('textarea');
                         element.value = this._textPlain;
                         this._cslBibRef.appendChild(element);
@@ -136,8 +122,24 @@ class CloudCiteBibliography extends HTMLElement {
                         document.execCommand('copy');
                         this._cslBibRef.removeChild(element);
                     });
+                    
+                    for (let i = 0; i < ProjectStore.project.citations.length; i++) {
+                        this._cslBibRef.querySelector(`#copyCitationButton${i}`).addEventListener('click', (e) => {
+                            this._textPlain = this._cslBibRef.querySelector(`#cslEntryContainer${i}`).innerText;
+                            this._textHTML = `<div class="csl-bib-body" style="${this._format ? (this._format.linespacing != null ? (`line-height:${this._format.linespacing};`): ''): ''}${this._format ? (this._format.hangingindent != null ? (`text-indent:-${this._format.hangingindent}em;`): ''): ''}"><div style="clear:left;${(this._format.entryspacing != null ? (`margin-bottom:${this._format.entryspacing}em;"`): '"')}>${this._citationHTML[i]}</div></div>`;
+                            var element = document.createElement('textarea');
+                            element.value = this._textPlain;
+                            this._cslBibRef.appendChild(element);
+                            element.focus({
+                                preventScroll: true
+                            });
+                            element.setSelectionRange(0, element.value.length);
+                            document.execCommand('copy');
+                            this._cslBibRef.removeChild(element);
+                        });
+                    }
+                    return this._cslBibRef;
                 }
-                return this._cslBibRef;
             }
         }
     }
@@ -160,7 +162,7 @@ class CloudCiteBibliography extends HTMLElement {
             ${ProjectStore.project.citations.length == 0 ? this.html`<p>This bibliography looks a little empty. You can create a new citation from the menu.</p>`: ``}
             ${{
                 any: this.generatePreview(),
-                placeholder: this.html`<p>Loading...</p>`
+                placeholder: ProjectStore.project.citations.length > 0 ? this.html`<p>Loading...</p>`: ``
             }}
         </div>`;
     }

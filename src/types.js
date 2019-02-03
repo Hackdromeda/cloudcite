09f51576469bf54cb29cfcc2001f8009b45e20f8
Naval Patel <navalpatel384@gmail.com>
2019-02-02 23:14:07 -0800

    Added empty CiteForm component

---
+++ src/types.js
@@ -0,0 +1,142 @@
+export const types = [
+    {
+        "value": "graphic",
+        "text": "Artwork"
+    },
+    {
+        "value": "song",
+        "text": "Audio Recording"
+    },
+    {
+        "value": "bill",
+        "text": "Bill"
+    },
+    {
+        "value": "post-weblog",
+        "text": "Blog Post"
+    },
+    {
+        "value": "book",
+        "text": "Book"
+    },
+    {
+        "value": "chapter",
+        "text": "Book Chapter"
+    },
+    {
+        "value": "legal_case",
+        "text": "Case"
+    },
+    {
+        "value": "computerProgram",
+        "text": "Computer Program"
+    },
+    {
+        "value": "paper-conference",
+        "text": "Conference Paper"
+    },
+    {
+        "value": "entry-dictionary",
+        "text": "Dictionary Entry"
+    },
+    {
+        "value": "article",
+        "text": "Document"
+    },
+    {
+        "value": "personal_communication",
+        "text": "E-mail"
+    },
+    {
+        "value": "entry-encyclopedia",
+        "text": "Encyclopedia Article"
+    },
+    {
+        "value": "motion_picture",
+        "text": "Film"
+    },
+    {
+        "value": "post",
+        "text": "Forum Post"
+    },
+    {
+        "value": "hearing(bill)",
+        "text": "Hearing"
+    },
+    {
+        "value": "instantMessage(personal_communication)",
+        "text": "Instant Message"
+    },
+    {
+        "value": "interview",
+        "text": "Interview"
+    },
+    {
+        "value": "article-journal",
+        "text": "Journal Article"
+    },
+    {
+        "value": "letter(personal_communication)",
+        "text": "Letter"
+    },
+    {
+        "value": "article-magazine",
+        "text": "Magazine Article"
+    },
+    {
+        "value": "manuscript",
+        "text": "Manuscript"
+    },
+    {
+        "value": "map",
+        "text": "Map"
+    },
+    {
+        "value": "newspaper-article",
+        "text": "Newspaper Article"
+    },
+    {
+        "value": "article",
+        "text": "Note"
+    },
+    {
+        "value": "patent",
+        "text": "Patent"
+    },
+    {
+        "value": "song(podcast)",
+        "text": "Podcast"
+    },
+    {
+        "value": "speech",
+        "text": "Presentation"
+    },
+    {
+        "value": "broadcast",
+        "text": "Radio Broadcast"
+    },
+    {
+        "value": "report",
+        "text": "Report"
+    },
+    {
+        "value": "legislation",
+        "text": "Statute"
+    },
+    {
+        "value": "broadcast",
+        "text": "TV Broadcast"
+    },
+    {
+        "value": "thesis",
+        "text": "Thesis"
+    },
+    {
+        "value": "video(motion_picture)",
+        "text": "Video Recording"
+    },
+    {
+        "value": "webpage",
+        "text": "Website"
+    }
+];

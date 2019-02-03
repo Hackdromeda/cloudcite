4c460e2deee85a2c33146c84a350cba29c04617c
Matt Seto <seanseto@gmail.com>
2019-02-02 23:23:48 -0800

    Add Sass styling to Projects.

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { CREATE_PROJECT, DELETE_PROJECT, SELECT_PROJECT } from '../actions/projects';
-import { Grid, Card, Button } from 'semantic-ui-react';
-import './Projects.css';
+// import { Grid, Card, button } from 'semantic-ui-react';
+import './Projects.scss';
 const crypto = require('crypto');
 
 const mapStateToProps = state => ({
     projects: state.projectsReducer.projects,
     selectedProject: state.projectsReducer.selectedProject
 });
-  
+
 const mapDispatchToProps = dispatch => ({
     CREATE_PROJECT: (project) => dispatch(CREATE_PROJECT(project)),
     DELETE_PROJECT: (projectId) => dispatch(DELETE_PROJECT(projectId)),
@@ -39,32 +39,26 @@ class Projects extends Component {
 
         return (
             <div id="projects">
-                <Grid columns={4} padded>
-                    {this.props.projects.map(function(project, index) {
-                        return (
-                            <Grid.Column key={index}>
-                                <Card style={{minWidth: '30vh', minHeight: '30vh'}}>
-                                    <Card.Content header={project.title} />
-                                    <Card.Content style={{textAlign: 'left'}} description={`Style: ${project.style.value}`} />
-                                    <Card.Content extra>
-                                        <Button disabled={this.props.selectedProject !== project.id ? false: true} onClick={() => this.selectProject(project.id)}>Select</Button>
-                                        <Button>Edit</Button>
-                                        <Button negative disabled={this.props.selectedProject !== project.id ? false: true} onClick={() => this.deleteProject(project.id)}>Delete</Button>
-                                    </Card.Content>
-                                </Card>
-                            </Grid.Column>
-                        )
-                    }, this)}
-                    <Grid.Column>
-                        <Card style={{minWidth: '30vh', minHeight: '30vh'}}>
-                            <Card.Content header="Create New Project" />
-                            <Card.Content style={{textAlign: 'left'}} description={`Style: ${this.props.projects.find(project => project.id === this.props.selectedProject).style.value}`} />
-                            <Card.Content extra>
-                                <Button primary onClick={() => this.createProject()}>Create</Button>
-                            </Card.Content>
-                        </Card>
-                    </Grid.Column>
-                </Grid>
+                {this.props.projects.map(function (project, index) {
+                    return (
+                        <div className="project-card">
+                            <header className="card-header">{project.title}</header>
+                            <div className="project-card-content">Style: {this.props.projects.find(project => project.id === this.props.selectedProject).style.value}</div>
+                            <footer className="card-footer">
+                                <button disabled={this.props.selectedProject !== project.id ? false : true} onClick={() => this.selectProject(project.id)}>Select</button>
+                                <button>Edit</button>
+                                <button disabled={this.props.selectedProject !== project.id ? false : true} onClick={() => this.deleteProject(project.id)}>Delete</button>
+                            </footer>
+                        </div>
+                    )
+                }, this)}
+                <div className="project-card">
+                    <header className="card-header">Create New Project</header>
+                    <div className="project-card-content">Style: {this.props.projects.find(project => project.id === this.props.selectedProject).style.value}</div>
+                    <footer className="card-footer">
+                        <button className="create-button" onClick={() => this.createProject()}>Create</button>
+                    </footer>
+                </div>
             </div>
         )
     }

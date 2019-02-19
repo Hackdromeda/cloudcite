528d4ef0c123968bad5d799d563119c1933a73be
Naval Patel <navalpatel384@gmail.com>
2019-02-18 22:15:32 -0800

    Added loading status to website autofill button

--- src/WebsiteAutofill/WebsiteAutofill.js
+++ src/WebsiteAutofill/WebsiteAutofill.js
@@ -95,7 +95,7 @@ class WebsiteAutofill extends Component {
                 return (
                     <Form className="citeForm">
                         <Input onChange={(e) => this.setState({ websiteInputURL: e.target.value })} placeholder="Cite Website" disabled={this.state.loaderVisible} />
-                        <Button className="btn" onClick={() => this.citeURL(this.state.websiteInputURL)} type="submit" disabled={this.state.websiteInputURL === '' || this.state.loaderVisible}>Cite Website</Button>
+                        <Button className="btn" loading={this.state.loaderVisible} onClick={() => this.citeURL(this.state.websiteInputURL)} type="submit" disabled={this.state.websiteInputURL === '' || this.state.loaderVisible}>Cite Website</Button>
                     </Form>
                 );
             }

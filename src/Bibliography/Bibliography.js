5eaf87eedff6868cdf6e7ef19010f2e2998a6183
Naval Patel <navalpatel384@gmail.com>
2019-02-19 00:53:16 -0800

    added support for copying bibliography and citations and deleting citations

--- src/Bibliography/Bibliography.js
+++ src/Bibliography/Bibliography.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { generateHTML } from '../functions/generateHTML';
 import { DELETE_CITATION } from '../actions/projects';
 import './Bibliography.css';
-import * as hyperHTML from 'hyperhtml';
 
 const mapStateToProps = state => ({
     selectedProject: state.projectsReducer.selectedProject,
@@ -16,11 +15,16 @@ const mapDispatchToProps = dispatch => ({
     DELETE_CITATION: (PROJECT_ID, CITATION_ID) => dispatch(DELETE_CITATION(PROJECT_ID, CITATION_ID))
 });
 
+function toPlainText(richText) {
+    return richText.replace(/<(?:.|\n)*?>/gim, "").replace(/\/n/gi, "").trim();
+}
+
 //citation prop is an array containing citation data
 class Bibliography extends Component {
     constructor(props) {
         super(props);
         this.generatePreview();
+        this.bibliographyRef = React.createRef();
     }
 
     componentDidUpdate() {
@@ -28,36 +32,34 @@ class Bibliography extends Component {
     }
 
     state = {
+        format: null,
+        citationHTML: [],
         textPlain: "",
         textHTML: ""
     }
 
-    copyCSL(e, cslBiliographyRef) {
-        e.clipboardData.setData("text/plain", this.state.textPlain);
+    copyCSL(e) {
+        e.clipboardData.setData("text/plain", this.state.plain);
         e.clipboardData.setData("text/html", this.state.textHTML);
         e.preventDefault();
     }
 
-    copyBibliographyText(e, cslBibliographyRef, format, citationHTML) {
-        let bibliographyContent = "";
-        for (let i = 0; i < citationHTML.length; i++) {
-            bibliographyContent += cslBibliographyRef.querySelector(`#cslEntryContainer${i}`).innerText;
+    copyCitation(e, index) {
+        this.setState({
+            textPlain: toPlainText(this.state.citationHTML[index]),
+            textHTML:  `<div class="csl-bib-body" style="${this.state.format ? (this.state.format.linespacing != null ? (`line-height:${this.state.format.linespacing};`): ''): ''}${this.state.format ? (this.state.format.hangingindent != null ? (`text-indent:-${this.state.format.hangingindent}em;`): ''): ''}"><div style="clear:left;${(this.state.format.entryspacing != null ? (`margin-bottom:${this.state.format.entryspacing}em;"`): '"')}>${this.state.citationHTML[index]}</div></div>`
+        }, () => document.execCommand('copy'));
+    }
+
+    copyBibliography(e) {
+        let textPlain = "";
+        for (let i = 0; i < this.state.citationHTML.length; i++) {
+            textPlain += toPlainText(this.state.citationHTML[i]);
         }
-        let textPlain = bibliographyContent;
-        let textHTML = `<div class="csl-bib-body" style="${format ? (format.linespacing != null ? (`line-height:${format.linespacing};`): ''): ''}${format ? (format.hangingindent != null ? (`text-indent:-${format.hangingindent}em;`): ''): ''}">${citationHTML.map(cslHTMLItem => `<div style="clear:left;${(format.entryspacing != null ? (`margin-bottom:${format.entryspacing}em;"`): '"')}>${cslHTMLItem}</div>`).join('')}</div>`;
         this.setState({
             textPlain: textPlain,
-            textHTML: textHTML
-        });
-        let element = document.createElement('textarea');
-        element.value = textPlain;
-        cslBibliographyRef.appendChild(element);
-        element.focus({
-            preventScroll: true
-        });
-        element.setSelectionRange(0, element.value.length);
-        document.execCommand('copy');
-        cslBibliographyRef.removeChild(element);
+            textHTML: `<div class="csl-bib-body" style="${this.state.format ? (this.state.format.linespacing != null ? (`line-height:${this.state.format.linespacing};`): ''): ''}${this.state.format ? (this.state.format.hangingindent != null ? (`text-indent:-${this.state.format.hangingindent}em;`): ''): ''}">${this.state.citationHTML.map(cslHTMLItem => `<div style="clear:left;${(this.state.format.entryspacing != null ? (`margin-bottom:${this.state.format.entryspacing}em;"`): '"')}>${cslHTMLItem}</div>`).join('')}</div>`
+        }, () => document.execCommand('copy'));
     }
 
     deleteCitation(id) {
@@ -70,66 +72,46 @@ class Bibliography extends Component {
             console.log(generatedHTML.error)
         } 
         else {
-            let format = generatedHTML.format;
-            if (generatedHTML.html && generatedHTML.html.length > 0) {
-                let citationHTML = generatedHTML.html.map(htmlItem => htmlItem.html);
-                let cslBibliographyRef = hyperHTML.wire()`
-                        <div oncopy="${(e) => this.copyCSL(e, cslBibliographyRef)}" style="text-align: center; justify-content: center;">
-                            <div id="bibliographyActions">
-                                <span id="copyBibliographyText" onclick="${(e) => this.copyBibliographyText(e, cslBibliographyRef, format, citationHTML)}" style="cursor: pointer;">Copy Bibliography <i class="copy icon"></i></span>
-                            </div>
-                            <div style=${{marginTop: '10px', backgroundColor: '#ffffff', border: '1px solid #e0e0e0', padding: '20px', borderRadius: '5px', textAlign: 'left', fontWeight: 'normal !important'}}>
-                                <div class="csl-bib-body" style=${{lineHeight: format.linespacing, marginLeft: `${format.hangingindent}em`, textIndent: `-${format.hangingindent}em`}}>
-                                    ${citationHTML.map((cslEntry, index) =>
-                                        `<div id="cslEntryContainer${index}" style="clear: left; margin-bottom: ${format.entryspacing}}">
-                                            ${cslEntry}
-                                        </div>
-                                            <div style="text-align: right;">
-                                                    <span id="copyCitationButton${index}" class="bibliographyActionButton">
-                                                        <i class="copy outline icon"></i>
-                                                    </span>
-                                                    <span class="bibliographyActionButton">
-                                                        <i class="pencil alternate icon"></i>
-                                                    </span>
-                                                    <span class="bibliographyActionButton">
-                                                        <i class="trash icon"></i>
-                                                    </span>
-                                                </div>
-                                        </div>`
-                                    )}
-                                </div>
-                            </div>
-                        </div>`;                  
-                    for (let i = 0; i < generatedHTML.html.length; i++) {
-                        cslBibliographyRef.querySelector(`#copyCitationButton${i}`).addEventListener('click', (e) => {
-                            let textPlain = cslBibliographyRef.querySelector(`#cslEntryContainer${i}`).innerText;
-                            let textHTML = `<div class="csl-bib-body" style="${format ? (format.linespacing != null ? (`line-height:${format.linespacing};`): ''): ''}${format ? (format.hangingindent != null ? (`text-indent:-${format.hangingindent}em;`): ''): ''}"><div style="clear:left;${(format.entryspacing != null ? (`margin-bottom:${format.entryspacing}em;"`): '"')}>${citationHTML[i]}</div></div>`;
-                            this.setState({
-                                textPlain: textPlain,
-                                textHTML: textHTML
-                            })
-                            var element = document.createElement('textarea');
-                            element.value = textPlain;
-                            cslBibliographyRef.appendChild(element);
-                            element.focus({
-                                preventScroll: true
-                            });
-                            element.setSelectionRange(0, element.value.length);
-                            document.execCommand('copy');
-                            cslBibliographyRef.removeChild(element);
-                        });
-                    }
-                hyperHTML.bind(this.refs.bibliography)`${cslBibliographyRef}`;
-            }
+            this.setState({
+                format: generatedHTML.format,
+                citationHTML: generatedHTML.html.map(htmlItem => htmlItem.html)
+            });
         }
     }
 
     render() {
         return (
-            <div>
-                <div style={{marginTop: '10px', backgroundColor: '#ffffff', padding: '20px', textAlign: 'left', justifyContent: 'center', fontWeight: 'normal !important'}}>
-                    <div ref="bibliography"/>
-                </div>
+            <div style={{marginTop: '10px', backgroundColor: '#ffffff', padding: '20px', textAlign: 'left', justifyContent: 'center', fontWeight: 'normal !important'}}>
+                {
+                    this.state.format && this.state.citationHTML.length > 0 ?
+                    <div ref="bibliographyRef" onCopy={(e) => this.copyCSL(e)} style={{textAlign: 'center', justifyContent: 'center'}}>
+                        <div id="bibliographyActions">
+                            <span id="copyBibliographyText" onClick={(e) => this.copyBibliography(e)} style={{cursor: 'pointer'}}>Copy Bibliography <i className="copy icon"></i></span>
+                        </div>
+                        <div style={{marginTop: '10px', backgroundColor: '#ffffff', border: '1px solid #e0e0e0', padding: '20px', borderRadius: '5px', textAlign: 'left', fontWeight: 'normal !important'}}>
+                            <div className="csl-bib-body" style={{lineHeight: this.state.format.linespacing, marginLeft: `${this.state.format.hangingindent}em`, textIndent: `-${this.state.format.hangingindent}em`}}>
+                                {this.state.citationHTML.map((cslEntry, index) =>
+                                    <Fragment key={index}>
+                                    <div id={`cslEntryContainer${index}`} style={{clear: 'left', marginBottom: this.state.format.entryspacing}}>
+                                        <div dangerouslySetInnerHTML={{__html: cslEntry}}/>
+                                    </div>
+                                    <div style={{textAlign: 'right'}}>
+                                            <span id={`copyCitationButton${index}`} className="bibliographyActionButton">
+                                                <i className="copy outline icon" onClick={(e) => this.copyCitation(e, index)}></i>
+                                            </span>
+                                            <span className="bibliographyActionButton">
+                                                <i className="pencil alternate icon"></i>
+                                            </span>
+                                            <span className="bibliographyActionButton">
+                                                <i className="trash icon" onClick={(e) => this.deleteCitation(this.state.format.entry_ids[index][0])}></i>
+                                            </span>
+                                    </div>
+                                </Fragment>
+                                )}
+                            </div>
+                        </div>
+                    </div>: <div/>
+                }
             </div>
         )
     }

6f290a2f15adf397eab74b975e37a1bc03a1c33e
Naval Patel <navalpatel384@outlook.com>
2019-02-23 19:26:02 -0800

    Settings is now a functional component and dependencies were updated

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -6,22 +6,20 @@ const FavoriteStyleSearch = React.lazy(() => import('../FavoriteStyleSearch/Favo
 const LocaleSearch = React.lazy(() => import('../LocaleSearch/LocaleSearch.js'));
 const StyleSearch = React.lazy(() => import('../StyleSearch/StyleSearch.js'));
 
-class Settings extends Component {
-  render() {
-    return (
-      <div className="container">
-        <h1 id="settingsHeader"><Icon className="settings" /> Settings</h1>
-        <div className="options">
-          <label className="settingsLabel">Favorite Styles</label>
-          <FavoriteStyleSearch />
-          <label className="settingsLabel">Locales</label>
-          <LocaleSearch />
-          <label className="settingsLabel">Styles</label>
-          <StyleSearch />
-        </div>
+function Settings() {
+  return (
+    <div className="container">
+      <h1 id="settingsHeader"><Icon className="settings" /> Settings</h1>
+      <div className="options">
+        <label className="settingsLabel">Favorite Styles</label>
+        <FavoriteStyleSearch />
+        <label className="settingsLabel">Locales</label>
+        <LocaleSearch />
+        <label className="settingsLabel">Styles</label>
+        <StyleSearch />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Settings;

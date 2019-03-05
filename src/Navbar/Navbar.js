fa16a3b58ce267048252a62c40305562970e069c
Naval Patel <navalpatel384@outlook.com>
2019-03-05 00:23:30 -0800

    updated Router location

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,51 +1,30 @@
-import React, { lazy, Suspense, Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import React, { Component } from 'react';
 import { Icon } from 'semantic-ui-react';
-// import './navbar.scss';
+import { withRouter } from 'react-router-dom';
+import './_navbar.scss';
 
-const App = lazy(() => import("../App.js"));
-const Projects = lazy(() => import('../Projects/Projects.js'));
-const CiteForm = lazy(() => import('../CiteForm/CiteForm.js'));
-const EditForm = lazy(() => import('../EditForm/EditForm.js'));
-const WebsiteAutofill = lazy(() => import('../WebsiteAutofill/WebsiteAutofill.js'));
-const Settings = lazy(() => import('../Settings/Settings.js'));
+class Navbar extends Component {
 
-function loadingComponent(Component) {
-  return props => (
-    <Suspense fallback={<div />}>
-      <Component {...props} />
-    </Suspense>
-  );
+  navigateTo(url) {
+    this.props.history.push(url);
+  }
+
+  render() {
+    return (
+      <div id="navbar" className="nav">
+        <a className="page closeHam current" onClick={() => this.navigateTo('/')}>CloudCite</a>
+        <a className="page closeHam" onClick={() => this.navigateTo('/projects')}>Projects</a>
+        <a href="https://api.cloudcite.net/" target="_blank" className="closeHam" rel="noopener noreferrer">API</a>
+        <a href="https://cloudcite.net/blog" target="_blank" className="closeHam" rel="noopener noreferrer">Blog</a>
+        <a href="https://status.cloudcite.net/" target="_blank" className="closeHam" rel="noopener noreferrer">Status</a>
+        <a href="https://help.cloudcite.net/" target="_blank" className="closeHam" rel="noopener noreferrer">Help</a>
+        <a href="https://feedback.cloudcite.net/" target="_blank" className="closeHam" rel="noopener noreferrer">Feedback</a>
+        <a href="https://github.com/Hackdromeda/cloudcite/" target="_blank" className="closeHam" rel="noopener noreferrer">Contribute</a>
+        <a className="page closeHam" onClick={() => this.navigateTo('/settings')}>Settings</a>
+        <a id="burger"><Icon className="hamMenu bars" /></a>
+      </div>
+    );
+  }
 }
 
-function Navbar() {
-
-  return (
-    <Router>
-      <Fragment>
-        <div id="navbar" className="nav">
-          <Link className="page closeHam current" to="/">CloudCite</Link>
-          <Link className="page closeHam" to="/projects">Projects</Link>
-          <a href="https://api.cloudcite.net/" target="_blank" className="closeHam" rel="noopener noreferrer">API</a>
-          <a href="https://cloudcite.net/blog" target="_blank" className="closeHam" rel="noopener noreferrer">Blog</a>
-          <a href="https://status.cloudcite.net/" target="_blank" className="closeHam" rel="noopener noreferrer">Status</a>
-          <a href="https://help.cloudcite.net/" target="_blank" className="closeHam" rel="noopener noreferrer">Help</a>
-          <a href="https://feedback.cloudcite.net/" target="_blank" className="closeHam" rel="noopener noreferrer">Feedback</a>
-          <a href="https://github.com/Hackdromeda/cloudcite/" target="_blank" className="closeHam" rel="noopener noreferrer">Contribute</a>
-          <Link className="page closeHam" to="/settings">Settings</Link>
-          <a id="burger"><Icon className="hamMenu bars" /></a>
-        </div>
-        <Switch>
-          <Route path="/" exact component={loadingComponent(App)} />
-          <Route path="/projects" component={loadingComponent(Projects)} />
-          <Route path="/cite" component={loadingComponent(CiteForm)} />
-          <Route path="/edit" component={loadingComponent(EditForm)} />
-          <Route path="/website" component={loadingComponent(WebsiteAutofill)} />
-          <Route path="/settings" component={loadingComponent(Settings)} />
-        </Switch>
-      </Fragment>
-    </Router>
-  );
-}
-
-export default Navbar;
+export default withRouter(Navbar);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import * as serviceWorker from './serviceWorker';
 import './styles.css';
 import Navbar from "./Navbar/Navbar";
 
+const App = lazy(() => import("./App.js"));
+const Projects = lazy(() => import('./Projects/Projects.js'));
+const CiteForm = lazy(() => import('./CiteForm/CiteForm.js'));
+const EditForm = lazy(() => import('./EditForm/EditForm.js'));
+const WebsiteAutofill = lazy(() => import('./WebsiteAutofill/WebsiteAutofill.js'));
+const Settings = lazy(() => import('./Settings/Settings.js'));
+
+function loadingComponent(Component) {
+  return props => (
+    <Suspense fallback={<div />}>
+      <Component {...props} />
+    </Suspense>
+  );
+}
 
 ReactDOM.render(
     <Provider store={store}>
-        <Navbar />
+        <Router>
+        	<div>
+	        	<Navbar/>
+	        	<Switch>
+		            <Route path="/" exact component={loadingComponent(App)} />
+		            <Route path="/projects" component={loadingComponent(Projects)} />
+		            <Route path="/cite" component={loadingComponent(CiteForm)} />
+		            <Route path="/edit" component={loadingComponent(EditForm)} />
+		            <Route path="/website" component={loadingComponent(WebsiteAutofill)} />
+		            <Route path="/settings" component={loadingComponent(Settings)} />
+	         	</Switch>
+         	</div>
+        </Router>
     </Provider>,
     document.getElementById('root')
 );

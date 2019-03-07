fd3d2e1b7d0b3eed445f59e2dd11e5e745581e41
Matt Seto <seanseto@gmail.com>
2019-03-06 17:44:36 -0800

    Workaround for bibliography updating on style change.

--- src/FavoriteStyleSearch/FavoriteStyleSearch.js
+++ src/FavoriteStyleSearch/FavoriteStyleSearch.js
@@ -5,25 +5,27 @@ import { Dropdown } from 'semantic-ui-react';
 
 const mapStateToProps = state => ({
 	selectedProject: state.projectsReducer.selectedProject,
-  	projects: state.projectsReducer.projects,
-  	favoriteStyles: state.favoriteStylesReducer.favoriteStyles
+	projects: state.projectsReducer.projects,
+	favoriteStyles: state.favoriteStylesReducer.favoriteStyles
 });
 
 const mapDispatchToProps = dispatch => ({
-  SET_STYLE: (id, style) => dispatch(SET_STYLE(id, {key: style.key, value: style.value}))
+	SET_STYLE: (id, style) => dispatch(SET_STYLE(id, { key: style.key, value: style.value }))
 });
 
 class FavoriteStyleSearch extends Component {
 
 	handleChange(e, { value }) {
 		this.props.SET_STYLE(this.props.selectedProject, this.props.favoriteStyles.find(favoriteStyle => favoriteStyle.value === value));
+		// Temporary workaround for bibliography style updating.
+		window.location.reload(); // Refresh page on change.
 	}
 
-  	render() {
-	    return (
-	      <Dropdown placeholder={this.props.projects.find(project => project.id === this.props.selectedProject).style.value} search selection options={this.props.favoriteStyles.map(style => Object.assign(style, {text: style.value}))} onChange={(e, value) => this.handleChange(e, value)}></Dropdown>
-	    );
-  	}
+	render() {
+		return (
+			<Dropdown placeholder={this.props.projects.find(project => project.id === this.props.selectedProject).style.value} search selection options={this.props.favoriteStyles.map(style => Object.assign(style, { text: style.value }))} onChange={(e, value) => this.handleChange(e, value)}></Dropdown>
+		);
+	}
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(FavoriteStyleSearch);

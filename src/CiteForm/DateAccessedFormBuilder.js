3b04715e21234bc60d1d74f5f09366af4767bd33
Naval Patel <navalpatel384@gmail.com>
2019-03-15 00:29:03 -0700

    Updated DateAccessedFormBuilder and DateIssuedFormBuilder components to functional components

--- src/CiteForm/DateAccessedFormBuilder.js
+++ src/CiteForm/DateAccessedFormBuilder.js
@@ -1,25 +1,22 @@
-import React, { Component, Fragment } from 'react';
-import { Button, Form, Input, Dropdown } from 'semantic-ui-react';
+import React from 'react';
+import { Button, Dropdown, Form, Input } from 'semantic-ui-react';
 import { monthNames } from './monthNames.js';
 
-class DateAccessedFormBuilder extends Component {
-
-  render() {
-    return (
-      <Fragment>
-        <Form.Group>
-          <Form.Field control={Dropdown} selection lazyLoad label="Month Accessed" placeholder="Month Accessed" options={monthNames} value={parseInt(this.props.accessed.month) ? parseInt(this.props.accessed.month): this.props.accessed.month} onChange={(e, value) => this.props.changeDateAccessed(e, 'month', value)}/>
-          <Form.Field control={Input} label="Day Accessed" type="number" placeholder="Day Accessed" defaultValue={this.props.accessed.day} onChange={(e, value) => this.props.changeDateAccessed(e, 'day', value)}/>
-          <Form.Field control={Input}  label="Year Accessed" type="number" placeholder="Year Accessed" defaultValue={this.props.accessed.year} onChange={(e, value) => this.props.changeDateAccessed(e, 'year', value)}/>
-        </Form.Group>
-        <Button.Group>
-          <Button style={{backgroundColor: '#b71c1c', color: '#ffffff'}} onClick={() => this.props.removeDateAccessed()}>Remove Date</Button>
-          <Button.Or />
-          <Button style={{backgroundColor: '#005eea', color: '#ffffff'}} onClick={() => this.props.setDateAccessedToday()}>Add Today's Date</Button>
-        </Button.Group>
-      </Fragment>
-    );
-  }
+function DateAccessedFormBuilder(props) {
+  return (
+    <div>
+      <Form.Group>
+        <Form.Field control={Dropdown} selection lazyLoad label="Month Accessed" placeholder="Month Accessed" options={monthNames} value={parseInt(props.accessed.month) ? parseInt(props.accessed.month): props.accessed.month} onChange={(e, value) => props.changeDateAccessed(e, 'month', value)}/>
+        <Form.Field control={Input} label="Day Accessed" type="number" placeholder="Day Accessed" defaultValue={props.accessed.day} onChange={(e, value) => props.changeDateAccessed(e, 'day', value)}/>
+        <Form.Field control={Input}  label="Year Accessed" type="number" placeholder="Year Accessed" defaultValue={props.accessed.year} onChange={(e, value) => props.changeDateAccessed(e, 'year', value)}/>
+      </Form.Group>
+      <Button.Group>
+        <Button style={{backgroundColor: '#b71c1c', color: '#ffffff'}} onClick={() => props.removeDateAccessed()}>Remove Date</Button>
+        <Button.Or />
+        <Button style={{backgroundColor: '#005eea', color: '#ffffff'}} onClick={() => props.setDateAccessedToday()}>Add Today's Date</Button>
+      </Button.Group>
+    </div>
+  );
 }
 
 export default DateAccessedFormBuilder;
--- src/CiteForm/DateIssuedFormBuilder.js
+++ src/CiteForm/DateIssuedFormBuilder.js
@@ -1,25 +1,22 @@
-import React, { Component, Fragment } from 'react';
-import { Button, Form, Input, Dropdown } from 'semantic-ui-react';
+import React from 'react';
+import { Button, Dropdown, Form, Input } from 'semantic-ui-react';
 import { monthNames } from './monthNames.js';
 
-class DateIssuedFormBuilder extends Component {
-
-  render() {
-    return (
-      <Fragment>
-        <Form.Group>
-          <Form.Field control={Dropdown} selection lazyLoad label="Month Published" placeholder="Month Published" options={monthNames} value={parseInt(this.props.issued.month) ? parseInt(this.props.issued.month): this.props.issued.month} onChange={(e, value) => this.props.changeDateIssued(e, 'month', value)}/>
-          <Form.Field control={Input} label="Day Published" type="number" placeholder="Day Published" defaultValue={this.props.issued.day} onChange={(e, value) => this.props.changeDateIssued(e, 'day', value)}/>
-          <Form.Field control={Input} label="Year Published" type="number" placeholder="Year Published" defaultValue={this.props.issued.year} onChange={(e, value) => this.props.changeDateIssued(e, 'year', value)}/>
-        </Form.Group>
-        <Button.Group>
-          <Button style={{backgroundColor: '#b71c1c', color: '#ffffff'}} onClick={() => this.props.removeDateIssued()}>Remove Date</Button>
-          <Button.Or />
-          <Button style={{backgroundColor: '#005eea', color: '#ffffff'}} onClick={() => this.props.setDateIssuedToday()}>Add Today's Date</Button>
-        </Button.Group>
-      </Fragment>
-    );
-  }
+function DateIssuedFormBuilder() {
+  return (
+    <div>
+      <Form.Group>
+        <Form.Field control={Dropdown} selection lazyLoad label="Month Published" placeholder="Month Published" options={monthNames} value={parseInt(props.issued.month) ? parseInt(props.issued.month): props.issued.month} onChange={(e, value) => props.changeDateIssued(e, 'month', value)}/>
+        <Form.Field control={Input} label="Day Published" type="number" placeholder="Day Published" defaultValue={props.issued.day} onChange={(e, value) => props.changeDateIssued(e, 'day', value)}/>
+        <Form.Field control={Input} label="Year Published" type="number" placeholder="Year Published" defaultValue={props.issued.year} onChange={(e, value) => props.changeDateIssued(e, 'year', value)}/>
+      </Form.Group>
+      <Button.Group>
+        <Button style={{backgroundColor: '#b71c1c', color: '#ffffff'}} onClick={() => props.removeDateIssued()}>Remove Date</Button>
+        <Button.Or />
+        <Button style={{backgroundColor: '#005eea', color: '#ffffff'}} onClick={() => props.setDateIssuedToday()}>Add Today's Date</Button>
+      </Button.Group>
+    </div>
+  );
 }
 
 export default DateIssuedFormBuilder;

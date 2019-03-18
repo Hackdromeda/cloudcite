e463d179361336cd27f66d0702fc29463cc07c87
Naval Patel <navalpatel384@gmail.com>
2019-03-17 19:25:10 -0700

    PullTab improvements

--- src/Settings/PullTab.js
+++ src/Settings/PullTab.js
@@ -1,6 +1,5 @@
 import React, { Suspense, useState } from 'react'
-
-import { Icon } from 'semantic-ui-react';
+import { Button, Icon } from 'semantic-ui-react';
 
 const FavoriteStyleSearch = React.lazy(() => import('../FavoriteStyleSearch/FavoriteStyleSearch.js'));
 const LocaleSearch = React.lazy(() => import('../LocaleSearch/LocaleSearch.js'));
@@ -21,8 +20,10 @@ export default function PullTab(props) {
           (expanded) ? (
             <div className="settingsContainer">
             <h1 className="settingsHeader"><Icon className="settings" /> Settings</h1>
-            <button onClick={closeTab}><Icon name='close' color='blue'/></button>
             <div className="options">
+              <div>
+                <Button circular icon onClick={closeTab}><Icon name='close'/></Button>
+              </div>
               <label className="settingsLabel">Favorite Styles</label>
               <FavoriteStyleSearch/>
               <label className="settingsLabel">Locales</label>
@@ -34,10 +35,10 @@ export default function PullTab(props) {
           ):(
             <React.Fragment>
             <div id="pulltabBtns">
-              <a className='settingsBtn' rel="noopener noreferrer" href="#" ><Icon className='settingsIcon' name='theme' color='blue'/></a>
-              <a className='settingsBtn' rel="noopener noreferrer" href="#" ><Icon className='settingsIcon' name='settings' /></a>
-              <a className='settingsBtn' rel="noopener noreferrer" href="#" ><Icon className='settingsIcon' name='settings' /></a>
-              <a className='settingsBtn' rel="noopener noreferrer" href="#" ><Icon className='settingsIcon' name='settings' /></a>
+              <Button circular icon><Icon name='theme'/></Button>
+              <Button circular icon><Icon name='theme'/></Button>
+              <Button circular icon><Icon name='theme'/></Button>
+              <Button circular icon><Icon name='theme'/></Button>
             </div>
             <button id="settingsExpand" onClick={()=> setExpand(true)}>Expand</button>
           </React.Fragment>
